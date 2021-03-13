89865b5a4858a17d488dcb9136b6145921eeb3e3
Filipp <filippgerman32@gmail.com>
2021-03-13 15:29:17 +0300

    create Homework_3

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,27 @@
-import style from './style.module.css'
+import style from './style.module.css';
 
-const Layout = ({id, title, desc, urlBg = false, colorBg = false}) => {
+const Layout = ({id, title, urlBg = false, colorBg = false, children = false}) => {
     const styleBg = {}; 
     if (urlBg) { styleBg.backgroundImage = `url(${urlBg})` }
     if (colorBg) { styleBg.backgroundColor = colorBg }
     return (
-        <>
-            <section 
-                className={style.root} 
-                style={styleBg}
-            >
-                <div className={style.wrapper}>
-                    <article>
-                        <div className={style.title}>
-                            <h3>{title}</h3>
-                            <span className={style.separator}></span>
-                        </div>
-                        <div className={style.desc, style.full}>
-                            <p>{desc}</p>
-                        </div>
-                    </article>
-                </div>
-            </section>
-        </>
+        <section 
+            className={style.root} 
+            style={styleBg}
+            id={id}
+        >
+            <div className={style.wrapper}>
+                <article>
+                    <div className={style.title}>
+                        <h3>{title}</h3>
+                        <span className={style.separator}></span>
+                    </div>
+                    <div className={`${style.desc} ${style.full}`}>
+                        {children}
+                    </div>
+                </article>
+            </div>
+        </section>
     );
 }
 
